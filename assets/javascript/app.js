63d3f6bcde872397e5a8b856c9428aad5add91e1
Jessica Son <jessicason@Jessicas-MacBook-Pro.local>
2018-04-14 12:23:40 -0700

    adds blurb, date, location into firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -131,15 +131,27 @@ $(document).ready(function () {
 
 
     // jessica's code
-    var content; var currentTime = moment(currentTime).format("hh:mm a");
+   
     $("#edit-button").on("click", function (event) {
-        content = $("#comment-input").val().trim();
+        var currentTime = moment(currentTime).format("hh:mm a");
+        var content = $("#comment-input").val().trim();
+        var blurb = $("#blurb-input").val().trim();
+        var location = $("#location-input").val().trim();
+        var date = $("#date-input").val().trim();
+
         
         console.log("content: ", content);
         console.log("time: ", currentTime);
+        console.log ("blurb: ", blurb);
+        console.log( "location: ", location);
+        console.log ("date: ", date);
+       
        
         var newComment = {
             content: content,
+            blurb: blurb,
+            location: location,
+            date: date,
             time: currentTime
         };
         database.ref().push(newComment);
