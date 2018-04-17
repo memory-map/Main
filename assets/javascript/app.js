94256fa267a0d8c86f823e5fc299c13210cdc7b3
Sunny <sundeepkmanik@gmail.com>
2018-04-16 18:10:57 -0700

    transferred over design and reformatted code; NOTE THAT SOME CLASS NAMES HAVE  CHANGED

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 // ======= GLOBAL VARIABLES =======
 // ================================
 
-// Initialize Firebase
+// initializing firebase
 var config = {
     apiKey: "AIzaSyDHwZqvulvp_MbrSBxHDe3jjR1VF1PqCwo",
     authDomain: "memory-map-3b3c8.firebaseapp.com",
@@ -15,218 +15,215 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// variable for toggle
-var bubbleClose = true;
-var map, infoWindow;
+// variables for modes
+var isDisplayModeOn;
 
-// function that fixes init error
-function initMap() { };
+// variable for Google Maps
+var map, infoWindow;
 
 // =============================
 // ========== ACTIONS ==========
 // =============================
 
-$(document).ready(function () {
+// toggle functions
+function toggleDisplay() {
 
-    // =======================================================
-    // ===== GOOGLE MAPS ACTIONS GO WITHIN THIS FUNCTION =====
+    hideAllInfo();
 
-    initMap = function () {
+    isDisplayModeOn = true;
 
-        var defaultLoc = { lat: 37.8712, lng: -122.2727 };
-        
-        // declaring map object
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: defaultLoc,
-            zoom: 13,
-            fullscreenControl: false,
-            streetViewControl: false,
-            mapTypeControl: false,
-            styles: [
-                {
-                    featureType: "poi",
-                    stylers: [{ visibility: "off" }]
-                },
-                {
-                    featureType: "transit",
-                    stylers: [{ visibility: "off" }]
-                }
-            ]
-        });
+    console.log("Display mode activated!");
+    $("#edit-mode").removeClass("active");
+    $("#display-mode").addClass("active");
 
-        var input = document.getElementById('pac-input');
-        var autocomplete = new google.maps.places.Autocomplete(input);
-        
-        autocomplete.bindTo('bounds', map);
+}
 
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+function toggleEdit() {
 
-        var infowindow = new google.maps.InfoWindow();
-        var infowindowContent = document.getElementById('infowindow-content');
-        infowindow.setContent(infowindowContent);
-        var marker = new google.maps.Marker({
-            map: map
-        });
-        marker.addListener('click', function() {
-            infowindow.open(map, marker);
-        });
+    hideAllInfo();
 
-        autocomplete.addListener('place_changed', function() {
-            infowindow.close();
-            var place = autocomplete.getPlace();
-            if (!place.geometry) {
-                return;
+    isDisplayModeOn = false;
+
+    console.log("Edit mode activated!");
+    $("#display-mode").removeClass("active");
+    $("#edit-mode").addClass("active");
+
+}
+
+function hideAllInfo () {
+
+    console.log("Info hidden!");
+    $("#side-panel").removeClass("smenu-open").addClass("smenu-close");
+    $("#side-edit").removeClass("smenu-open").addClass("smenu-close");
+
+}
+
+// init Map function that MUST be declared globally, do not move into doc ready
+function initMap() {
+
+    // our default location: berkeley
+    var defaultLoc = { lat: 37.8712, lng: -122.2727 };
+
+    // declaring map object
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: defaultLoc,
+        zoom: 13,
+        fullscreenControl: false,
+        streetViewControl: false,
+        mapTypeControl: false,
+        styles: [
+            {
+                featureType: "poi",
+                stylers: [{ visibility: "off" }]
+            },
+            {
+                featureType: "transit",
+                stylers: [{ visibility: "off" }]
             }
+        ]
+    });
 
-            if (place.geometry.viewport) {
-                map.fitBounds(place.geometry.viewport);
-            } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(17);
-            }
-        });
+    // adding search bar
+    var input = document.getElementById("pac-input");
+    var autocomplete = new google.maps.places.Autocomplete(input);
 
-        var marker = new google.maps.Marker({
-            position: defaultLoc,
-            map: map,
-            title: 'Hello World!'
-        });
+    autocomplete.bindTo("bounds", map);
 
-        marker.addListener("click", togglePanel);
+    autocomplete.addListener("place_changed", function () {
 
-        google.maps.event.addListener(map, "click", function (e) {
-            var latLng = e.latLng;
-            // make new marker?
-            var clickMarker = new google.maps.Marker({
-                position: {lat: latLng.lat(), lng:latLng.lng()},
-                map: map,
-                
-            });
+        var place = autocomplete.getPlace();
 
-            var newLatitude = {
-                lat: latLng.lat(), 
-                lng: latLng.lng(),
-            }
+        if (!place.geometry) {
 
-            database.ref().push(newLatitude);
+            return;
 
-
-            map.panTo(clickMarker.getPosition());
-            console.log("did it move?");
-
-            console.log(clickMarker);
-            //lat and lng is available in e object
-
-            // permit side panel functionality on new pinpoints
-            clickMarker.addListener("click", togglePanel);
-            
-            console.log(latLng.lat());
-
-            var contentString = "<div class='clickme'>" + "Hi! There!" + "</div>";
-
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
-
-            clickMarker.addListener('click', function () {
-                infowindow.open(map, clickMarker);
-            });
-
-            $("#side-panel").removeClass("menu-open");
-            $("#side-panel").addClass("menu-close");
-        
-        });
-
-    }
-
-    function togglePanel() {
-
-        console.log("Hi!");
-
-        if (bubbleClose === true) {
-
-            $(".blurb-bubble").removeClass("b-close").addClass("b-open");
-
-            bubbleClose = false;
-
-        } else if (bubbleClose === false) {
-
-            $(".blurb-bubble").removeClass("b-open").addClass("b-close");
-
-            bubbleClose = true;
-
-            
-
-            /* if ($("#display-mode").hasClass("active")) {
-
-                console.log("Display mode has class active!");
-                $("#side-panel").removeClass("menu-close").addClass("menu-open");
-    
-            } else if ($("#edit-mode").hasClass("active")) {
-                console.log("Edit mode has class active!");
-                // open side edit when edit mode is active
-                $("#side-edit").removeClass("menu-close").addClass("menu-open");
-            }
- */
         }
 
-    }
+        if (place.geometry.viewport) {
 
-    // this shit fucks up easily so instead write it so that
-    // one click on pinpoint displays blurb, second click on pinpoint removes blurb and shows side-panel. capisce
-    $(document.body).on("click", ".clickme", function () {
+            map.fitBounds(place.geometry.viewport);
 
-        /* console.log("Blurb clicked!");
-        $("#side-panel").removeClass("menu-close");
-        $("#side-panel").addClass("menu-open"); */
-        console.log("Blurb clicked!");
-        if ($("#display-mode").hasClass("active")) {
+        } else {
 
-            console.log("Display mode has class active!");
-            $("#side-panel").removeClass("menu-close").addClass("menu-open");
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
 
-        } else if ($("#edit-mode").hasClass("active")) {
-            console.log("Edit mode has class active!");
-            // open side edit when edit mode is active
-            $(".side-edit").removeClass("menu-close").addClass("menu-open");
         }
 
     });
 
-    /* $(".blurb-bubble").on("click", function () {
+    $(".close-button").on("click", function () {
+
+        $("#side-panel").removeClass("smenu-open").addClass("smenu-close");
+        $("#side-edit").removeClass("smenu-open").addClass("smenu-close");
+        console.log("sidebar closed");
+
+    })
+
+    // adding click event that generates new markers
+    google.maps.event.addListener(map, "click", function (e) {
+
+        // makes so this only runs in edit mode
+        if (isDisplayModeOn === true) {
+
+            return;
+
+        }
+
+        //lat and lng is available in e object
+        var latLng = e.latLng;
+
+        // make new marker
+        var clickMarker = new google.maps.Marker({
+            position: { lat: latLng.lat(), lng: latLng.lng() },
+            map: map,
+
+        });
+
+        var newLatitude = {
+            lat: latLng.lat(),
+            lng: latLng.lng(),
+        }
+
+        database.ref().push(newLatitude);
+
+        map.panTo(clickMarker.getPosition());
+        console.log("did it move?");
+
+        console.log(clickMarker);
+        console.log(latLng.lat());
+
+        // info window stuff
+        var contentString = "<div class='clickme'>" + "Hi! There!" + "</div>";
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        clickMarker.addListener("click", function () {
+
+            if (isDisplayModeOn === true) {   
+
+                infowindow.open(map, clickMarker);
+
+            } else if (isDisplayModeOn === false) {
+
+                $("#side-edit").removeClass("smenu-close").addClass("smenu-open");
+
+            }
+
+        });
+
+        // whatever panel open, close them
+        hideAllInfo();
+
+    });
+
+};
+
+// =====================================================
+// === DOCUMENT READY: ONLY FOR NON GOOGLE MAP STUFF ===
+// =====================================================
+
+$(document).ready(function () {
+
+    $(document.body).on("click", ".clickme", function () {
 
         console.log("Blurb clicked!");
+
         if ($("#display-mode").hasClass("active")) {
 
             console.log("Display mode has class active!");
-            $("#side-panel").removeClass("menu-close").addClass("menu-open");
+
+            // open side info when display mode is active
+            $("#side-panel").removeClass("smenu-close").addClass("smenu-open");
 
         } else if ($("#edit-mode").hasClass("active")) {
+
             console.log("Edit mode has class active!");
+
             // open side edit when edit mode is active
-            $(".side-edit").removeClass("menu-close").addClass("menu-open");
+            $("#side-edit").removeClass("smenu-close").addClass("smenu-open");
+
         }
 
-    }); */
-
+    });
 
     // jessica's code
-   
     $("#edit-button").on("click", function (event) {
+
         var currentTime = moment(currentTime).format("hh:mm a");
         var content = $("#comment-input").val().trim();
         var blurb = $("#blurb-input").val().trim();
         var location = $("#location-input").val().trim();
         var date = $("#date-input").val().trim();
 
-        
         console.log("content: ", content);
         console.log("time: ", currentTime);
-        console.log ("blurb: ", blurb);
-        console.log( "location: ", location);
-        console.log ("date: ", date);
-       
-       
+        console.log("blurb: ", blurb);
+        console.log("location: ", location);
+        console.log("date: ", date);
+
         var newComment = {
             content: content,
             blurb: blurb,
@@ -234,72 +231,19 @@ $(document).ready(function () {
             date: date,
             time: currentTime
         };
+
         database.ref().push(newComment);
+
     });
-
-    // =======================================
-    // ===== OUR UI CLICK EVENTS GO HERE =====
-
-    // allows pinpoint to be clicked
-    // $("#pinpoint-placeholder").on("click", function () {
-
-    //     console.log("Pinpoint clicked!", "Bubble status: ", bubbleClose);
-
-    //     if (bubbleClose === true) {
-
-    //         $(".blurb-bubble").removeClass("b-close").addClass("b-open");
-
-    //         bubbleClose = false;
-
-    //         $(".blurb-bubble").on("click", function () {
-
-    //             console.log("Blurb clicked!");
-    //             $("#side-panel").removeClass("menu-close");
-    //             $("#side-panel").addClass("menu-open");
-
-    //         });
-
-    //     } else if (bubbleClose === false) {
-
-    //         $(".blurb-bubble").removeClass("b-open").addClass("b-close");
-
-    //         bubbleClose = true;
-
-    //         $("#side-panel").removeClass("menu-open");
-    //         $("#side-panel").addClass("menu-close");
-
-    //     }
-
-    // });
 
     // allows display mode to be activated
-    $("#display-mode").on("click", function () {
-
-        console.log("Display mode activated!");
-        $("#edit-mode").removeClass("active");
-        $(this).addClass("active");
-
-        
-
-    });
+    $("#display-mode").on("click", toggleDisplay);
 
     // allows edit mode to be activated
-    $("#edit-mode").on("click", function () {
+    $("#edit-mode").on("click", toggleEdit);
 
-        console.log("Edit mode activated!");
-        $("#display-mode").removeClass("active");
-        $(this).addClass("active");
-
-        
-
-    });
-
-
-    $(".close-button").on("click", function () {
-        $("#side-panel").removeClass("menu-open").addClass("menu-close");
-        $(".side-edit").removeClass("menu-open").addClass("menu-close");
-        console.log("sidebar closed");
-    })
+    // initialize toggle
+    toggleDisplay();
 
     // document ready closing tag
 });
